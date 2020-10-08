19cd449350fd6b79507f13c353646328e0502de4
WendyFu004 <71946736+WendyFu004@users.noreply.github.com>
2020-10-07 23:39:34 -0400

    Create Player.h

---
+++ comp345_assignment1/Part1/Player.h
@@ -0,0 +1,49 @@
+#ifndef DEMO11_PLAYER_H
+#define DEMO11_PLAYER_H
+
+#include <stdio.h>
+#include <iostream>
+#include <list>
+#include <vector>
+#include "Map.h"
+//#include "Cards.h"
+#include "Orders.h"
+
+
+class Player
+{
+private:
+	 //int counter;
+public:
+	string* name;
+	vector<Territory*> territories;
+	vector<Order*> orders;
+	
+	Player(); // default constructor
+	Player(string pname);
+	Player( Player& p, string pname);
+	~Player(); // destructor
+
+	void setTerr(vector<Territory*>);
+	string getName();
+	void printOrder();
+
+	void issueOrder();
+	vector<Territory*> toDefend();
+	vector<Territory*> toAttack();
+
+	Player& operator =(const Player& p);
+	friend ostream& operator <<(ostream &strm, Player &player);
+};
+
+//class Order
+//{
+//public:
+//	string* orderlines;
+//
+//	Order();
+//	Order(string o);
+//	string getOrder();
+//
+//};
+#endif DEMO11_PLAYER_H
