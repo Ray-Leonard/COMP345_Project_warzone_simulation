f19657f341cbf5b0ee98b0a5d253543c41ecf683
Wencent <72359340+Wencent@users.noreply.github.com>
2020-10-12 18:46:33 -0400

    Update OrderDriver.cpp

--- comp345_assignment1/Part1/OrderDriver.cpp
+++ comp345_assignment1/Part1/OrderDriver.cpp
@@ -1,4 +1,4 @@
-
+#define _CRTDBG_MAP_ALLOC
 #define ORDERS_H
 #ifdef ORDERS_H
 #include <string>
@@ -6,27 +6,28 @@
 #include "Map.h"
 #include "Orders.h"
 #include "Player.h"
+#include <crtdbg.h>
+
 using namespace std;
 
 int main() {
     // Create some players and territories to do the tests
-    Player* player = new Player{"player1"};
-    Player* opponent = new Player("player2");
+ /*   Player* player = new Player{"player1"};
+    Player* opponent = new Player("player2");*/
     
-    Territory* playerTerritory2 = new Territory{ "Canada",4 };
+    /*Territory* playerTerritory2 = new Territory{ "Canada",4 };
     Territory* playerTerritory = new Territory{ "American",5 };
-    Territory* opponentTerritory = new Territory{ "Cuba",3 };
+    Territory* opponentTerritory = new Territory{ "Cuba",3 };*/
 
     // Test the orders
     cout << "Testing the orders:" << endl << endl;
-    Order* deploy = new Deploy(player, playerTerritory);
-    Order* advance = new Advance(player, playerTerritory, opponentTerritory);
-    Order* bomb = new Bomb(player, playerTerritory, opponentTerritory);
-    Order* blockade = new Blockade(player, playerTerritory2);
-    Order* negotiate = new Negotiate(player, opponent);
-    Order* airlift = new Airlift(player, playerTerritory, opponentTerritory);
+    Order* deploy = new Deploy();
+    Order* advance = new Advance();
+    Order* bomb = new Bomb();
+    Order* blockade = new Blockade();
+    Order* negotiate = new Negotiate();
+    Order* airlift = new Airlift();
 
-    
 
     cout << "Deploy is valid: " << deploy->validate() << endl;
     deploy->execute(); 
@@ -49,9 +50,9 @@ int main() {
 
     // Test the orders list
     cout << endl
-        << "===========================" << std::endl
-        << std::endl
-        << "Test the orderslist" << std::endl;
+        << "===========================" << endl
+        << endl
+        << "Test the orderslist" << endl;
 
     OrdersList* ordersList = new OrdersList();
     ordersList->queue(deploy);
@@ -61,7 +62,7 @@ int main() {
     ordersList->queue(negotiate);
     ordersList->queue(airlift);
 
-    std::cout << *ordersList << std::endl;
+    cout << *ordersList << endl;
 
     // Move orders within the list
     // Expected results:  Deploy, Negotiate, Advance, Blockade, Bomb, Airlift
@@ -81,19 +82,34 @@ int main() {
     // Finish the tests, delete everything
 
     delete ordersList;
-    delete player;
+    /*delete player;
     delete opponent;
-    delete playerTerritory;
     delete opponentTerritory;
-
+    delete playerTerritory;
+    delete playerTerritory2;*/
+    /*delete deploy;*/
+    /*delete airlift;*/
+   /* delete advance;
+    delete bomb;
+    delete blockade;
+    delete negotiate;
+    */
     
     // eliminate danling pointer
     ordersList = nullptr;
-    player = nullptr;
+    /*player = nullptr;
     opponent = nullptr;
-    playerTerritory = nullptr;
     opponentTerritory = nullptr;
+    playerTerritory = nullptr;
+    playerTerritory2 = nullptr;*/
+    deploy = nullptr;
+    airlift = nullptr;
+    advance = nullptr;
+    bomb = nullptr;
+    blockade = nullptr;
+    negotiate = nullptr;
 
+    _CrtDumpMemoryLeaks();
     return 0;
 };
 #endif
