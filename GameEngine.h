9cdda5f6712c92dc4f19cd25b6a2996e3839f876
Ray-Leonard <58916847+Ray-Leonard@users.noreply.github.com>
2020-11-11 09:04:50 -0600

    Add files via upload

---
+++ GameEngine.h
@@ -0,0 +1,64 @@
+//
+// Created by Nian Liu on 2020-10-19.
+//
+
+#ifndef untitled_GAMEENGINE_H
+#define untitled_GAMEENGINE_H
+#include <string>
+#include <iostream>
+#include "Map.h"
+#include "MapLoader.h"
+#include "Player.h"
+#include "Cards.h"
+using namespace std;
+
+class GameEngine {
+private:
+    Map* m;
+    int numOfPlayers;
+    bool observerFlag;
+    vector<Player*> pl;
+    Deck* deck;
+
+    // allows the game engine to read a map from user input and gracefully reject invalid map files
+    void readMap();
+    // allows the user to set the number of territories
+    void setNumOfPlayer();
+    // allows the user to choose whether to turn on observer
+    void setObserverSwitch();
+    // helps to determine the order in which players play
+    void shufflePlayers();
+    // shows in which order that players are playing
+    void printPlayerSequence();
+    // prints a snapshot of how many armies each player have in their army pool
+    void printPlayerArmy();
+    // prints a snapshot of which territory each player has
+    void printPlayerTerritory();
+
+
+public:
+    // constructors
+    GameEngine();
+    GameEngine(Map& map, int players, bool observerSwitch, Deck* _deck);
+    GameEngine(const GameEngine& gameEngine);
+
+    // destructor
+    ~GameEngine();
+    /* 
+      The loading phase does the following:
+    *   1. Ask the user to input a map name
+    *   2. Check map validity, if invalid, go back to step 1
+    *   3. Ask the user to select the # of players
+    *   4. Check the validity of # of players, if invalid, go back to step 3
+    *   5. Create players based on the # of players, and assign each of them an empty hand of cards(being done in Player constructor)
+    *   6. Create a Deck
+    *   7. Ask the user to turn on/off observer
+    *   8. (optional) print all the informations up to now
+    */
+    void loadingPhase();
+    void startupPhase();
+
+};
+
+
+#endif 
---
+++ Map.h
@@ -0,0 +1,133 @@
+//
+// Created by Nian Liu on 2020-10-02.
+//
+
+#ifndef untitled_MAP_H
+#define untitled_MAP_H
+
+#include <string>
+#include <list>
+#include <array>
+#include <map>
+#include <vector>
+using namespace std;
+
+class Territory
+{
+    string tName; //territory name
+    int tID; //territory id
+    int cID; //continent id this territory belongs to
+    string cName; //continent name this territory belongs to
+    int pID; //player id
+    int armyNum; //num of army 
+public:
+    vector<int> adjacentTerritoryVec; //adjacent territories vector for current territory
+    /*
+    subgraph, contains relationship between territories in the current continent
+    each element in allTerritories is a Territory obj, which contains adjacentTerritoryVec vector
+    */
+    vector<int> allAdjWithinSameConti;
+
+    //default constructor
+    Territory();
+
+    //parameterized constructor
+    Territory(string tName, int tID, int cID, string cName);
+    //copy constructor
+    Territory(const Territory &obj);
+    //destructor
+    ~Territory();
+
+    string gettName();
+    int gettId();
+    int getcId();
+    int getpId();
+    int getArmyNum();
+    vector<int> getAdjacentTerritoryVec();
+    vector<int> getAllAdjWithinSameConti();
+    void setAdjTrriWithinSameConti(); //set allAdjWithinSameConti
+    void setName(string tName);
+    void settId(int tId);
+    void setcId(int cId);
+    void setpId(int pId);
+    void setArmyNum(int armyNum);
+    void addAdjacentTerritory(int t);
+    void printAdjacentTerritoryVec();
+    void printAllAdjWithinSameConti();
+
+    friend ostream& operator <<(ostream& output, Territory& obj);
+};
+
+class Continent
+{
+    string continentName;
+    int cId; //continent id
+    int bonusArmyNum; //# of army a player will get if he/she occupies all countries in current continent
+    /*
+    subgraph, contains relationship between territories in the current continent
+    each element in allTerritories is a Territory obj, which contains adjacentTerritoryVec vector
+    so allTerritories is a 2D vector
+    note: the territories in adjacentTerritoryVec may exist in different continent
+    */
+    vector<int> allTerritories;
+
+
+public:
+    Continent();
+    Continent(int cId, string cName, int bonusArmyNum);
+    Continent(const Continent &obj);
+    ~Continent();
+
+    string getContinentName();
+    int getcId();
+    int getBounusArmyNum();
+    int getNumOfTerritories();
+    void setContinentName(string cName);
+    void setcId(int cId);
+    void setBounusArmyNum(int bonusArmyNum);
+    void addTerritory(int t);
+    bool validateSubgraph();
+    map<int, bool> contiDFS(int start_node);
+    void contiTraverseFunc(int start_node, map<int, bool>& tID_Visited);
+
+    friend ostream& operator <<(ostream& output, Continent& obj);
+
+};
+
+class Map
+{
+public:
+    static vector<Territory*> allNodes;
+    static vector<Continent*> allContinents;
+    int numOfTrritories;
+    int numOfContinents;
+
+    Map();
+    Map(int terri_num, int conti_num);
+    Map(const Map& obj);
+    ~Map();
+    int getNumOfTrritories();
+    int getNumOfContinents();
+    void setNumOfContinents(int conti_num);
+    void setNumOfTrritories(int terri_num);
+    void addContinent(Continent* c);
+    void addNode(Territory* tt);
+    static Continent* getContinentById(int cId);
+    static Territory* getTerritoryById(int tID);
+
+    void printNodes();
+    bool* mapDFS(int start_node, const int nodes_num); //pass the start node
+    //node: the start node, visited: an array to mark if a node is visited or not, vec: adjacency vector
+    void mapTraverseFunc(int start_node, bool visited[]);
+    bool isConnected(int nodes_num); //nodes_num: total number of nodes
+
+    //check if the map is connected graph
+    bool validate();
+    void printGraph();
+
+    //output a table
+    friend ostream& operator <<(ostream& output, Map& map_obj);
+};
+
+
+#endif //DEMO11_MAP_H
